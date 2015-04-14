389bed7906cf8f8df6c802a33af90f1884eef2fd
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-04-14 17:42:57 +0200

    Removed Textarea and Datepicker input.
    It was a workaround the fact tcomb-conditional-forms did not support
    passing options to the underlying tcomb-form. Now that it does, we can
    remove these inputs.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
-exports.Datepicker       = require("./inputs/datepicker.jsx");
 exports.List             = require("./inputs/list.jsx");
 exports.SimpleStringList = require("./inputs/simple-string-list.jsx");
-exports.Textarea         = require("./inputs/textarea.jsx");
